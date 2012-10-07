a2c06947f6b85985b7fb7c3f8e98a47622ede26e
ale <ale@vm3.ath.cx>
2012-10-07 22:50:22 +0200

    added tweeting

--- web.js
+++ web.js
@@ -1,11 +1,20 @@
-// TODO: implement HDEL
 var express = require('express');
 var app = express.createServer(express.logger());
-// these string are used while generating the management page; there's one for each in case you want to run commands on different servers
-var whost = "http://herojson.heroku.com"; // change to your webapp's address. If running locally this would be 127.0.0.1:3000
-var xhost = "http://herojson.heroku.com"; // same as above
-var checkinsHashName = "checkins_e";
-var debug = 1;
+var OAuth = require('oauth').OAuth;
+// these two string are used while generating the management page; there's one for each in case you want to run commands on different servers
+var whost = "http://herojson.herokuapp.com"; // change to your webapp's address. If running locally this would be 127.0.0.1:3000
+var xhost = "http://herojson.herokuapp.com"; // same as above
+twitterAccessTokenSecret = "change with your own twitter Oauth credentials"; // change with your own twitter Oauth credentials
+twitterAccessToken = "change with your own twitter Oauth credentials";
+twitterConsumerKey = "change with your own twitter Oauth credentials";
+twitterConsumerSecret = "change with your own twitter Oauth credentials";
+controlledHostsHashName = "controlledHosts"; // this is the redis hashname where information on hostnames under watch and their maximum allowed elapsed time between checkins. Time is expressed in seconds.
+var checkinsHashName = "checkins_e"; // this is the redis hashname where checkin timestamps are kept (hostname is the key within the hash)
+timeoutCheckInterval = 60000 ; interval length, in milliseconds, between host checks.
+var debug = 0;
+
+// TODO: implement HDEL, DEL
+
 if (process.env.REDISTOGO_URL) {
     var rtg = require("url")
         .parse(process.env.REDISTOGO_URL);
@@ -16,6 +25,52 @@ if (process.env.REDISTOGO_URL) {
     var redis = require("redis")
         .createClient();
 }
+
+// Helper function to send tweets
+SendTwit = function(messageStr) {
+    oAuth = new OAuth("http://twitter.com/oauth/request_token", "http://twitter.com/oauth/access_token",
+    twitterConsumerKey, twitterConsumerSecret, "1.0A", null, "HMAC-SHA1");
+    var date = new Date();
+    d = date.getTime();
+    oAuth.post("http://api.twitter.com/1/statuses/update.json",
+    twitterAccessToken, twitterAccessTokenSecret, {
+        "status": messageStr + " " + date
+    },
+
+    function(error, data) {
+        if (error) console.log(require('sys')
+            .inspect(error))
+        else console.log(data)
+    });
+    console.log("done sending");
+}
+// Function which gets called every minute or so to check for host timeouts
+CheckForTimeouts = function() {
+    var date = new Date();
+    d = date.getTime();
+    redis.hgetall(controlledHostsHashName, function(err, cHosts) {
+        redis.hgetall(checkinsHashName, function(err, checkins) {
+            console.log(err);
+            for (ea in cHosts) {
+                if ((d - checkins[ea]) > (cHosts[ea] * 1000)) {
+                    SendTwit("host: " + ea + " hasnt checked in in " + Math.round((d - checkins[ea]) / 1000) + " seconds. This is above the allowed maximum of " + cHosts[ea] + " seconds");
+                }
+            }
+        });
+    });
+}
+
+// Actual startup code
+
+CheckForTimeouts(); // running it once at startup to facilitate debugging, may want to comment it out
+x = setInterval(CheckForTimeouts, timeoutCheckInterval);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+// The following are the usual express routes, most of which are just wrappers around actual redis commands.
+
 app.get('/', function(request, response) {
     var date = new Date();
     var current_hour = date.getHours();
@@ -75,6 +130,9 @@ app.get('/CHECKIN/:what', function(req, res) {
         });
     });
 });
+
+// The following routes actually output html and are used for the rudimentary web GUI
+
 app.all('/redishash/:hashname', function(req, res) {
     response = "";
     if (debug) console.log("redishash/ " + hashname);
@@ -102,9 +160,9 @@ app.all('/check/:whatever', function(req, res) {
 });
 app.post('/redispost/:key/:field', function(req, res) {
     if (debug) console.log("posting to redis " + key + " " + field);
-    if (debug) console.log(req.body.contenuto);
-    // contenuto must be valid JSON
-    redis.hset(req.params.key, req.params.field, eval(req.body.contenuto), function(err, quotes) {
+    if (debug) console.log(req.body.contents);
+    // contents must be valid JSON, this has never been tested and will most likely fail
+    redis.hset(req.params.key, req.params.field, eval(req.body.contents), function(err, quotes) {
         res.send("OK" + req.params + "<br>");
     });
 });
@@ -124,7 +182,6 @@ app.all('/redis', function(req, res) {
         });
     });
 });
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    if (debug) console.log("Listening on " + port);
-});
+
+
+
